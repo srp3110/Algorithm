41b1181c1c25dd3fed3817524d6eb7ed4204dc82
srp3110 <surrya20@gmail.com>
2022-12-10 06:11:21 +0800

    set 5 part 1

---
+++ Algo-Written-Test-2/Set 6.py
@@ -0,0 +1,23 @@
+"""Question 1"""
+
+# num = int(input('Enter a non-negative number => '))
+
+# if num < 0:
+#     print('Enter a non-negative number')
+# else:
+#     if num % 2 == 0:
+#         print(f'{num} is an even number')
+#     else:
+#         print(f'{num} is an odd number')
+
+"""Question 6"""
+
+# list_num = [90, 34, 67, 12, 59]
+
+# def sum(list):
+#     if len(list) == 1:
+#         return list[0]
+#     else:
+#         return list[0] + sum(list[1:])
+
+# print(sum(list_num))
